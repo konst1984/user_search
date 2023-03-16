02f71f44b0059a3eae5a2979ab749ac4a3401203
Konst_V <varjutin@gmail.com>
2023-03-16 17:45:28 +0300

    create search

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import { ReactComponent as SearchIcon } from "assets/icon-search.svg";
+import styles from "./Search.module.scss";
+import { Button } from "../Button";
+
+interface SearchProps {
+  hasError: boolean;
+  onSubmit: (text: string) => void;
+}
+
+type FormFields = {
+  username: HTMLInputElement;
+};
+
+export const Search = ({ hasError, onSubmit }: SearchProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement & FormFields>) => {
+    e.preventDefault();
+    const text = e.currentTarget.username.value;
+    if (text) {
+      onSubmit(text);
+      e.currentTarget.reset();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <div className={styles.search}>
+        <label htmlFor="search" className={styles.label}>
+          <SearchIcon />
+        </label>
+        <input
+          type="text"
+          className={styles.textField}
+          id="search"
+          name="username"
+          placeholder="Search GitHub username..."
+        />
+        {hasError && <div className={styles.error}>No result</div>}
+        <Button>Search</Button>
+      </div>
+    </form>
+  );
+};
